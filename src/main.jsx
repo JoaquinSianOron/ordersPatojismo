4f277dbaa4fff6d2140f7b38e56d8d582b6bd1ab
Marcos F3rnando Morales <125113310+Marcos-F-Morales@users.noreply.github.com>
2023-08-29 15:59:35 -0600

    StyleandComponet
    se empezo con la estructura

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react' 
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import HomeKitchen from './components/homeKitchen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HomeKitchen></HomeKitchen>
   </React.StrictMode>,
 )
