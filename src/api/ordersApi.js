ffb062638453125e2c47f35d7e71ea33bef6a51f
Joaquin Sian <oscarjoaquinsian@gmail.com>
2023-10-12 14:40:53 -0600

    actualizacion dataorders

--- src/api/ordersApi.js
+++ src/api/ordersApi.js
@@ -3,8 +3,11 @@ import axios from "axios";
 const getAllFood = () => {
     
     return axios.get("https://backendkitchenapp.onrender.com/foods/food/")
-   //https://backendkitchenapp.onrender.com/foods/food/
+    
+    //backendkitchenapp.onrender.com/foods/food/
    //https://backendkitchenapp.onrender.com/orders/order/
-} 
+
+}
+
 
 export default getAllFood;
--- src/components/dataOrders.jsx
+++ src/components/dataOrders.jsx
@@ -1,40 +1,43 @@
-import React, { useEffect } from 'react'
-import getAllFood from '../api/ordersApi'
-import { useState } from 'react';
-import {} from "../assets/css/cardMenu.css"
+import React, { useEffect } from "react";
+import { useState } from "react";
+import getAllFood from "../api/ordersApi";
+import {} from "../assets/css/cardMenu.css";
+
 
 function dataOrders() {
-const [orders, setOrders] = useState([]);
+  
+  const [orders, setOrders] = useState([]);
 
-useEffect(() => {
-  async function loadOrders() {
-    const res = await getAllFood();
-    setOrders(res.data);
-    console.log(res)
-  }
-  loadOrders();
-}, []);
+  useEffect(() => {
+    async function loadOrders() {
+      const res = await getAllFood();
+      setOrders(res.data);
+      console.log(res);
+    }
+    loadOrders();
+  }, []);
 
+  return (
+    <div>
+      {orders.map((food) => (
+        <div className="cardOrders">
+          <div key={food.id}>
+            <h1>
+              {food.name} - Q. {food.price} .00
+            </h1>
+          </div>
 
-  return <div>
+          <button className="bg-AA8787  text-black font-Lalezar sm-w-147 py-2 px-1 mt-1/2 rounded">
+            Ordenar
+          </button>
 
-{orders.map(food => (
-  <div className='cardOrders'>
-        <div key={food.id}>
-          <h1>{food.name} - Q. {food.price} .00</h1> 
-        
-        </div>
-        <button className="bg-AA8787  text-black font-Lalezar sm-w-147 py-2 px-1 mt-1/2 rounded">
-  Ordenar
-</button>
         </div>
       ))}
-
-
-
     </div>
-  
+  );
 }
 
-export default dataOrders
- 
+export default dataOrders;
+
+
+
